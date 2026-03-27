3f4f1f05f3fc1e743aef00e22aeab351e339554b
zaraiwassim <wassimzarai28@gmail.com>
2026-03-27 22:28:49 +0100

    fixing nabar

--- src/app/features/back-office/components/admin-sidebar/admin-sidebar.component.ts
+++ src/app/features/back-office/components/admin-sidebar/admin-sidebar.component.ts
@@ -1,45 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../../../services/auth.service';
-import { UserService } from '../../../../services/user.service';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-admin-sidebar',
     templateUrl: './admin-sidebar.component.html',
     styleUrls: ['./admin-sidebar.component.scss']
 })
-export class AdminSidebarComponent implements OnInit {
-    firstName: string = 'Admin';
-    initials: string = 'AD';
+export class AdminSidebarComponent {
 
-    constructor(private authService: AuthService, private userService: UserService) { }
-
-    ngOnInit() {
-        this.loadUserInfo();
-        this.userService.getProfile().subscribe({
-            next: (user) => {
-                if (user && user.fullName) {
-                    this.setNames(user.fullName);
-                }
-            }
-        });
-    }
-
-    private loadUserInfo() {
-        const fullName = this.authService.getUserFullName();
-        if (fullName) {
-            this.setNames(fullName);
-        }
-    }
-
-    private setNames(fullName: string) {
-        if (!fullName) return;
-        const parts = fullName.split(' ');
-        this.firstName = parts[0];
-        this.initials = parts.map(n => n ? n[0] : '').join('').toUpperCase();
-        if (!this.initials) this.initials = this.firstName[0].toUpperCase();
-    }
+    constructor(private router: Router) { }
 
     logout() {
-        this.authService.logout();
+        this.router.navigate(['/front']);
     }
+
 }
--- src/app/features/back-office/components/admin-topbar/admin-topbar.component.ts
+++ src/app/features/back-office/components/admin-topbar/admin-topbar.component.ts
@@ -1,41 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../../../services/user.service';
-import { AuthService } from '../../../../services/auth.service';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-admin-topbar',
     templateUrl: './admin-topbar.component.html',
     styleUrls: ['./admin-topbar.component.scss']
 })
-export class AdminTopbarComponent implements OnInit {
-    firstName: string = 'Admin';
-    initials: string = 'AD';
+export class AdminTopbarComponent {
 
-    constructor(private userService: UserService, private authService: AuthService) { }
-
-    ngOnInit() {
-        this.loadUserInfo();
-        this.userService.getProfile().subscribe({
-            next: (user) => {
-                if (user && user.fullName) {
-                    this.setNames(user.fullName);
-                }
-            }
-        });
-    }
-
-    private loadUserInfo() {
-        const fullName = this.authService.getUserFullName();
-        if (fullName) {
-            this.setNames(fullName);
-        }
-    }
-
-    private setNames(fullName: string) {
-        if (!fullName) return;
-        const parts = fullName.split(' ');
-        this.firstName = parts[0];
-        this.initials = parts.map(n => n ? n[0] : '').join('').toUpperCase();
-        if (!this.initials) this.initials = this.firstName[0].toUpperCase();
-    }
 }
